98687c69209fb9931e9fc2da4a4bf44e94533899
taslimy <taslimer@gmail.com>
2019-04-11 19:25:13 -0400

    fixed a small error

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -5,9 +5,7 @@ class TabLink {
     this.data = this.element.dataset.tab;
     //console.log(this.element.dataset.tab);
     // Using the custom data attribute get the associated Item element
-    this.itemElement = document.querySelector(
-      `.tabs-item[data-tab='${this.data}']`
-    );
+    this.itemElement = document.querySelector( `.tabs-item[data-tab='${this.data}']`);
     console.log(this.itemElement);
     // Using the Item element, create a new instance of the TabItem class
     this.tabItem = new TabItem(this.itemElement);
