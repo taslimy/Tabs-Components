9388b1270feab13b7c6b151bd5d46d9069a5d693
taslimy <taslimer@gmail.com>
2019-04-11 19:23:20 -0400

    Fixed some comments

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -20,6 +20,7 @@ class TabLink {
     // Get all of the elements with the tabs-link class
     const links = document.querySelectorAll(".tabs-link");
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
+    // Array.from(links).forEach();
     // The Array.from() method creates a new, shallow - copied Array instance from an array - like or iterable object.
     Array.from(links).forEach(function(link) {
       link.classList.remove("tabs-link-selected");
